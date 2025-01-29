4913923cbf2bfa69eaf33b5564e7f7ff1012f723
DABILGOU <dabilgou10@gmail.com>
2025-01-29 19:29:48 +0000

    hello

--- src/app/gestion.guard.ts
+++ src/app/gestion.guard.ts
@@ -7,7 +7,7 @@ export const gestionGuard: CanActivateFn = (route, state) => {
   const _service = inject(WenService);
   const _auth_service = inject(AuthenService);
   const router = inject(Router);
-  if (_auth_service.userSignal() && (_auth_service.userSignal()?.role == "admin" ||_auth_service.userSignal()?.role == "user1")) {
+  if (_auth_service.userSignal()) {
     return true;
   } else {
 
